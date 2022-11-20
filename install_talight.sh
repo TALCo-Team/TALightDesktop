9543c81b6d9e9372e86a27dfd7f483bdef540c81
Cesare Montresor <cesare.montresor@gmail.com>
2022-11-20 11:24:19 +0100

    - added TALight installation script

---
+++ install_talight.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+PROJECT_FOLDER="TALight"
+CURRENT_DIR=`pwd`
+
+sudo apt install cargo
+
+git clone https://github.com/romeorizzi/TALight.git ./$PROJECT_FOLDER
+
+
+export TAL_HOME="$CURRENT_DIR/$PROJECT_FOLDER"
+
+if ! grep -i "export TAL_HOME" ~/.bashrc; then
+    echo "Adding paths to ~/.bashrc"
+    echo "export TAL_HOME=$TAL_HOME" >> ~/.bashrc
+    echo "export PATH=\"\$PATH:$TAL_HOME/rtal/target/debug\"" >> ~/.bashrc
+    echo "export PATH=\"\$PATH:$TAL_HOME/rtal/target/release\"" >> ~/.bashrc   
+fi
+
+export PATH="$PATH:$TAL_HOME/rtal/target/debug"
+export PATH="$PATH:$TAL_HOME/rtal/target/release"
+
+cd $PROJECT_FOLDER
+pwd
+cd rtal 
+cargo build
+cargo build --release
+cd ..
+
+
+
+
