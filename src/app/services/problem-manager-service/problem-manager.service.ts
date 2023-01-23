70fc84b3db2d40312d84a9275db81dcc1528a301
Cesare Montresor <cesare.montresor@gmail.com>
2023-01-23 17:11:23 +0100

    - improving args panel - improving url input field

--- src/app/services/problem-manager-service/problem-manager.service.ts
+++ src/app/services/problem-manager-service/problem-manager.service.ts
@@ -143,5 +143,42 @@ export class ProblemManagerService {
   }
 
 
+  validateArgs(service: ServiceDescriptor){
+    let issues = new Map<string,any>();
+    service.args.forEach((arg)=>{
+      let issue = this.validateArg(arg)
+      if(issue){issues.set(arg.key,issue)}
+    })
+    return issues;
+  }
+
+  validateArg(arg: ArgDescriptor){
+      //set
+      if (arg.regex === null){
+        console.log("validateArg:regex:null")
+        return null
+      }
+
+      //valid re
+      let pattern;
+      try{
+        pattern = new RegExp(arg.regex)
+      }catch(error:any) { 
+        console.log("validateArg:regex:invalid")
+        return null;
+      }
+
+      //match
+      let match = arg.value.match(pattern)
+      console.log("validateArg:value:",arg.value)
+      console.log("validateArg:pattern:",pattern)
+      console.log("validateArg:match:",match)
+      if(!match || match.length == 0){
+        console.log("validateArg:match:no")
+        return arg.name + ": Validation error";
+      }
+      console.log("validateArg:match:yes")
+      return null;
+  }
 
 }
